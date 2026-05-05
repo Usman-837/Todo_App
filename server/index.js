0806acf732ae547ab1b902406a2963b80509d65a
Usman <usmantufail837@gmail.com>
2026-05-05 18:07:41 +0500

    create new folders client and server and also install and make necessary files and folders in server side and create server and also connect db

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import app from "./app.js";
+import dotenv from 'dotenv';
+import mongoose from "mongoose";
+
+dotenv.config();
+
+// mongoDB Connection
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => {
+    console.log('MongoDB Connect')
+})
+.catch(() => {
+    console.log('MongoDB Connection Error', error);
+});
+
+const port = process.env.PORT || 5001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
+
