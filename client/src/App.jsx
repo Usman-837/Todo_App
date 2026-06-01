a555d300660df2f3740dcac23838fdc79def4c91
Usman <usmantufail837@gmail.com>
2026-06-01 21:14:07 +0500

    update statusCard and app components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,104 @@
-import React from 'react';
-import { Users, Plus } from 'lucide-react';
+import React, { useEffect, useState } from 'react';
+import { Users, User, Plus, Phone } from 'lucide-react';
 import StatusCard from './components/StatusCard';
 import SearchBar from './components/SearchBar';
 import UserTable from './components/UserTable';
 import UserModel from './components/UserModel';
+import { getUsers, searchUsers, getStatus, addUser, updateUser, deleteUser } from './api/UserApi';
 
 const App = () => {
+
+  const [users, setUsers] = useState([]);
+  const [totalUsers, setTotalUsers] = useState(0);
+  const [status, setStatus] = useState({ total: 0, active: 0, inactive: 0 });
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isModelOpen, setIsModelOpen] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    status: 'active',
+  });
+
+  const [editingItem, setEditingItem] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerpage, setItemesPerPage] = useState(5);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const stats = ['Active', 'Inactive'];
+
+  // fetch users
+  useEffect(() => {
+    fetchUsers();
+  }, [currentPage, itemsPerpage]);
+
+  useEffect(() => {
+    if (searchTerm) handleSearch();
+    else fetchUsers();
+  }, [searchTerm]);
+
+  const handleSubmit = async () => {
+    if (!formData.name || !formData.email || !formData.phone)
+      return alert('Fill all fields');
+    setLoading(true);
+
+    try {
+      if (editingItem) await updateUser(editingItem._id, formData);
+      else await addUser(formData);
+      fetchUsers();
+      closeModel();
+    } catch (error){
+      alert(error.message);
+    }
+    setLoading(false);
+  };
+
+  const handleDelete = async(id) => {
+    if(window.confirm('Are you sure')){
+      await deleteUser(id);
+      fetchUsers();
+    }
+  };
+
+  const openModel = (item = null) => {
+    if (item) {
+      setEditingItem(item);
+      setFormData(item);
+    } else {
+      setEditingItem(null);
+      setFormData({ name: '', email: '', phone: '', status: 'Active'});
+    }
+    setIsModelOpen(true);
+  };
+
+  const closeModel = () => {
+    setIsModelOpen(false);
+    setEditingItem(null);
+    setFormData({ name: '', email: '', phone: '', status: 'Active'});
+  };
+
+  // fetch status
+  const fetchStatus = async () => {
+    const data = await getStatus();
+    setStatus(data);
+  };
+
+  const fetchUsers = async () => {
+    const data = await getUsers(currentPage, itemsPerpage);
+    setUsers(data.users);
+    setTotalPages(data.totalPages);
+    setTotalUsers(data.setTotalUsers);
+    fetchStatus();
+  };
+
+  const handleSearch = async() => {
+    const data = await searchUsers(searchTerm, currentPage, itemsPerpage);
+    setUsers(data.users);
+    setTotalPages(data.totalPages);
+    setTotalUsers(data.totalPages);
+  };
+
   return (
     <div className='min-h-screen bg-gray-950'>
       {/* Header */}
@@ -20,7 +113,7 @@ const App = () => {
               <p className='text-gray-400 mt-1'>MERN Stack Application</p>
             </div>
           </div>
-          <button className='flex items-center gap-2 bg-green-500 text-gray-900 px-5 py-2.5 rounded-lg hover:bg-reen-400 transition-colors shadow-lg font-semibold'>
+          <button className='flex items-center gap-2 bg-green-500 text-gray-900 px-5 py-2.5 rounded-lg hover:bg-reen-400 transition-colors shadow-lg font-semibold' onClick={() => openModel()}>
             <plus size={20}/> Add User
           </button>
         </div>
@@ -29,14 +122,14 @@ const App = () => {
       <main className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8'>
         <div className='grid grid-col-1 md:grid-col-3 gap-6 mb-8'>
           {/* Status */}
-          <StatusCard/>
+          <StatusCard title='Total Users' value={{ number: status.total }} icon={<Users/>} bgIcon='bg-indigo-500' iconColor='text-white' gradient='from-indgo-900 to-indgo-700' />
         </div>
         {/* Search */}
         <SearchBar/>
         {/* User Table */}
         <UserTable/>
         {/* User Model */}
-        {/* <UserModel/> */}
+        <UserModel isOpen={isModelOpen} onClose={closeModel} />
       </main>
     </div>
   )
--- client/src/components/UserModel.jsx
+++ client/src/components/UserModel.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Check, X } from 'lucide-react'
 
-const UserModel = () => {
+const UserModel = ({ isOpen, onClose }) => {
+    if (!isOpen) return null; 
     return (
         <div className='fixed inset-0 bg-black/80 flex items-center justify-center p-4 z-50'>
             <div className='bg-gray-900 rounded-lg shadow-2xl max-w-2xl w-full max-h-screen overflow-y-auto border border-gray-800 '>
                 <div className='flex justify-between items-center p-6 border-b border-gray-800'>
                     <h2 className='text-2xl font-bold text-white'>Add New User</h2>
-                    <button className='text-gray-400 hover:text-white transition-all'><X size={24} /></button>
+                    <button className='text-gray-400 hover:text-white transition-all' onClick={onClose} ><X size={24} /></button>
                 </div>
                 <div className='p-6'>
                     <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
